dad7ad0d92c421a72cc334a1911a41e63d5850db
misterrat <cris_cgc_22@hotmail.com>
2022-04-24 13:12:07 +0200

    configuración paypal

---
+++ app/src/main/java/com/android/liveconcerts/Config/Config.kt
@@ -0,0 +1,6 @@
+package com.android.liveconcerts.Config
+
+object Config {
+    const val PAYPAL_CLIENT_ID =
+        "AQYLivPPqOtPvOojtWCOV37J3H1Sg4lHG2_5G7VyoDUussgc-joq7_oqB5Kn3fsfT6DTkQItv2AnIT11"
+}
