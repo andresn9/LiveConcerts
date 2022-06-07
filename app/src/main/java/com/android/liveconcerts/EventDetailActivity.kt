490b2208e5d623f5e0e52bbafb26fc52e5e5934a
misterrat <cris_cgc_22@hotmail.com>
2022-06-08 00:18:13 +0200

    ciclo con paypal completado (cambiar libreria paypal deprecated)

--- app/src/main/java/com/android/liveconcerts/EventDetailActivity.kt
+++ app/src/main/java/com/android/liveconcerts/EventDetailActivity.kt
@@ -1,10 +1,15 @@
 package com.android.liveconcerts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.liveconcerts.databinding.ActivityEventDetailBinding
 import com.android.liveconcerts.databinding.ActivityLoginBinding
 import com.android.liveconcerts.objects.Artist
+import com.paypal.android.sdk.payments.PayPalPayment
+import com.paypal.android.sdk.payments.PayPalService
+import com.paypal.android.sdk.payments.PaymentActivity
+import java.math.BigDecimal
 
 class EventDetailActivity : AppCompatActivity() {
 
@@ -21,6 +26,13 @@ class EventDetailActivity : AppCompatActivity() {
 
             text.text = artist.name
             image.setImageResource(artist.image)
+        }
+
+        binding.btnPaypal.setOnClickListener{
+
+            var intent = Intent (this, PayPalActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
